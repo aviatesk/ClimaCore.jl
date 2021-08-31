dbb0f135c35017f861aeed979a43e4380101923a
Valeria Barra <valeriabarra21@gmail.com>
2021-08-31 15:12:53 -0700

    Rebase onto main after PR #137 on vertex iterator and remove WarpMesh work for now

--- test/tensorproductmesh.jl
+++ test/tensorproductmesh.jl
@@ -160,3 +160,120 @@ end
               [(5, 4), (6, 4)]
     end
 end
+
+@testset "simple rectangular grid vertex iterator" begin
+    @testset "1×1 element quad mesh with all periodic boundries" begin
+        _, _, ts_topology = tensorproduct_grid(1, 1, true, true)
+        @test length(Topologies.vertices(ts_topology)) == 1
+        V = collect(Topologies.vertices(ts_topology))
+        @test V[1] isa Topologies.Vertex
+        @test length(V[1]) == 4
+        @test collect(V[1]) == [(1, 1), (1, 2), (1, 3), (1, 4)]
+    end
+
+    @testset "1×1 element quad mesh with 1 periodic boundary" begin
+        _, _, ts_topology = tensorproduct_grid(1, 1, true, false)
+        @test length(Topologies.vertices(ts_topology)) == 2
+        V = collect(Topologies.vertices(ts_topology))
+        @test V[1] isa Topologies.Vertex
+        @test length(V[1]) == 2
+        @test collect(V[1]) == [(1, 1), (1, 2)]
+        @test length(V[2]) == 2
+        @test collect(V[2]) == [(1, 3), (1, 4)]
+    end
+
+    @testset "1×1 element quad mesh with non-periodic boundaries" begin
+        _, _, ts_topology = tensorproduct_grid(1, 1, false, false)
+        @test length(Topologies.vertices(ts_topology)) == 4
+        V = collect(Topologies.vertices(ts_topology))
+        @test V[1] isa Topologies.Vertex
+        @test length(V[1]) == 1
+        @test collect(V[1]) == [(1, 1)]
+        @test length(V[2]) == 1
+        @test collect(V[2]) == [(1, 2)]
+        @test length(V[3]) == 1
+        @test collect(V[3]) == [(1, 3)]
+        @test length(V[4]) == 1
+        @test collect(V[4]) == [(1, 4)]
+    end
+
+    @testset "2×3 element quad mesh with non-periodic boundaries" begin
+        _, _, ts_topology = tensorproduct_grid(2, 3, false, false)
+        @test length(Topologies.vertices(ts_topology)) == 3 * 4
+        V = collect(Topologies.vertices(ts_topology))
+        @test length(V[1]) == 1
+        @test collect(V[1]) == [(1, 1)]
+    end
+
+    @testset "2×3 element quad mesh with periodic boundaries" begin
+        _, _, ts_topology = tensorproduct_grid(2, 3, true, true)
+        @test length(Topologies.vertices(ts_topology)) == 2 * 3
+        V = collect(Topologies.vertices(ts_topology))
+        @test length(V) == 6
+        @test collect(V[1]) == [(1, 1), (2, 2), (5, 3), (6, 4)]
+        @test collect(V[2]) == [(2, 1), (1, 2), (6, 3), (5, 4)]
+        @test collect(V[3]) == [(3, 1), (4, 2), (1, 3), (2, 4)]
+        @test collect(V[4]) == [(4, 1), (3, 2), (2, 3), (1, 4)]
+        @test collect(V[5]) == [(5, 1), (6, 2), (3, 3), (4, 4)]
+        @test collect(V[6]) == [(6, 1), (5, 2), (4, 3), (3, 4)]
+    end
+end
+
+@testset "simple rectangular grid coordinates" begin
+    @testset "1×1 element quad mesh with all periodic boundries" begin
+        _, _, ts_topology = tensorproduct_grid(1, 1, true, true)
+        c1, c2, c3, c4 = Topologies.vertex_coordinates(ts_topology, 1)
+        @test c1 == Cartesian2DPoint(0.0, 0.0)
+        @test c2 == Cartesian2DPoint(1.0, 0.0)
+        @test c3 == Cartesian2DPoint(0.0, 1.0)
+        @test c4 == Cartesian2DPoint(1.0, 1.0)
+
+        _, _, ts_topology = tensorproduct_grid(
+            1,
+            1,
+            false,
+            false;
+            x1min = -1.0,
+            x1max = 1.0,
+            x2min = -1.0,
+            x2max = 1.0,
+        )
+        c1, c2, c3, c4 = Topologies.vertex_coordinates(ts_topology, 1)
+        @test c1 == Cartesian2DPoint(-1.0, -1.0)
+        @test c2 == Cartesian2DPoint(1.0, -1.0)
+        @test c3 == Cartesian2DPoint(-1.0, 1.0)
+        @test c4 == Cartesian2DPoint(1.0, 1.0)
+    end
+
+    @testset "2×4 element quad mesh with non-periodic boundaries" begin
+        _, _, ts_topology = tensorproduct_grid(2, 4, false, false)
+        c1, c2, c3, c4 = Topologies.vertex_coordinates(ts_topology, 1)
+        @test c1 == Cartesian2DPoint(0.0, 0.0)
+        @test c2 == Cartesian2DPoint(0.5, 0.0)
+        @test c3 == Cartesian2DPoint(0.0, 0.25)
+        @test c4 == Cartesian2DPoint(0.5, 0.25)
+
+        c1, c2, c3, c4 = Topologies.vertex_coordinates(ts_topology, 8)
+        @test c1 == Cartesian2DPoint(0.5, 0.75)
+        @test c2 == Cartesian2DPoint(1.0, 0.75)
+        @test c3 == Cartesian2DPoint(0.5, 1.0)
+        @test c4 == Cartesian2DPoint(1.0, 1.0)
+    end
+
+    @testset "check coordinate type accuracy" begin
+        _, _, ts_topology = tensorproduct_grid(
+            3,
+            1,
+            false,
+            false,
+            x1min = big(0.0),
+            x1max = big(1.0),
+            x2min = big(0.0),
+            x2max = big(1.0),
+        )
+        c1, c2, c3, c4 = Topologies.vertex_coordinates(ts_topology, 1)
+        @test eltype(c2) == BigFloat
+        @test c2.x1 ≈ big(1.0) / big(3.0) rtol = eps(BigFloat)
+        @test c2.x2 == 0.0
+    end
+end
