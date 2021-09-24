34ab6bae190468850e39a058dc4ccd152a0851b2
Akshay Sridhar <asridhar@caltech.edu>
2021-09-24 13:59:33 -0700

    Update advection test cases to include flux-correction checks Add step function advection 	modified:   advect.jl 	new file:   step.jl

--- examples/column/step.jl
+++ examples/column/step.jl
@@ -19,10 +19,14 @@ global_logger(TerminalLogger())
 
 const FT = Float64
 
-a = FT(0.0)
-b = FT(4pi)
-n = 128
+a = FT(-20.0)
+b = FT(20.0)
+n = 64
 α = FT(0.1)
+
+function heaviside(t)
+    0.5 * (sign(t) + 1)
+end
 
 domain = Domains.IntervalDomain(a, b, x3boundary = (:left, :right))
 mesh = Meshes.IntervalMesh(domain, nelems = n)
@@ -31,43 +35,55 @@ cs = Spaces.CenterFiniteDifferenceSpace(mesh)
 fs = Spaces.FaceFiniteDifferenceSpace(cs)
 
 V = Geometry.Cartesian3Vector.(ones(FT, fs))
-θ = sin.(Fields.coordinate_field(cs))
+θ = heaviside.(Fields.coordinate_field(cs))
 
 # Solve advection Equation: ∂θ/dt = -∂(vθ)
 
 # upwinding
 function tendency1!(dθ, θ, _, t)
-    fcc = Operators.FluxCorrectionC2C(left=Operators.Extrapolate(), 
-                                      right=Operators.Extrapolate())
-    fcf = Operators.FluxCorrectionF2F(left=Operators.Extrapolate(), 
-                                      right=Operators.Extrapolate())
+    fcc = Operators.FluxCorrectionC2C(
+        left = Operators.Extrapolate(),
+        right = Operators.Extrapolate(),
+    )
+    fcf = Operators.FluxCorrectionF2F(
+        left = Operators.Extrapolate(),
+        right = Operators.Extrapolate(),
+    )
     UB = Operators.UpwindBiasedProductC2F(
         left = Operators.SetValue(sin(a - t)),
         right = Operators.SetValue(sin(b - t)),
     )
     ∂ = Operators.DivergenceF2C()
 
-    return @. dθ = -∂(UB(V, θ)) 
+    return @. dθ = -∂(UB(V, θ))
 end
 function tendency2!(dθ, θ, _, t)
-    fcc = Operators.FluxCorrectionC2C(left=Operators.Extrapolate(), 
-                                      right=Operators.Extrapolate())
-    fcf = Operators.FluxCorrectionF2F(left=Operators.Extrapolate(), 
-                                      right=Operators.Extrapolate())
+    fcc = Operators.FluxCorrectionC2C(
+        left = Operators.Extrapolate(),
+        right = Operators.Extrapolate(),
+    )
+    fcf = Operators.FluxCorrectionF2F(
+        left = Operators.Extrapolate(),
+        right = Operators.Extrapolate(),
+    )
     UB = Operators.UpwindBiasedProductC2F(
         left = Operators.SetValue(sin(a - t)),
         right = Operators.SetValue(sin(b - t)),
     )
     ∂ = Operators.DivergenceF2C()
-    return @. dθ = -∂(UB(V, θ)) + fcc(V,θ)
+    return @. dθ = -∂(UB(V, θ)) + fcc(V, θ)
 end
 # use the advection operator
 function tendency3!(dθ, θ, _, t)
 
-    fcc = Operators.FluxCorrectionC2C(left=Operators.Extrapolate(), 
-                                      right=Operators.Extrapolate())
-    fcf = Operators.FluxCorrectionF2F(left=Operators.Extrapolate(), 
-                                      right=Operators.Extrapolate())
+    fcc = Operators.FluxCorrectionC2C(
+        left = Operators.Extrapolate(),
+        right = Operators.Extrapolate(),
+    )
+    fcf = Operators.FluxCorrectionF2F(
+        left = Operators.Extrapolate(),
+        right = Operators.Extrapolate(),
+    )
     A = Operators.AdvectionC2C(
         left = Operators.SetValue(sin(-t)),
         right = Operators.Extrapolate(),
@@ -77,15 +93,19 @@ end
 # use the advection operator
 function tendency4!(dθ, θ, _, t)
 
-    fcc = Operators.FluxCorrectionC2C(left=Operators.Extrapolate(), 
-                                      right=Operators.Extrapolate())
-    fcf = Operators.FluxCorrectionF2F(left=Operators.Extrapolate(), 
-                                      right=Operators.Extrapolate())
+    fcc = Operators.FluxCorrectionC2C(
+        left = Operators.Extrapolate(),
+        right = Operators.Extrapolate(),
+    )
+    fcf = Operators.FluxCorrectionF2F(
+        left = Operators.Extrapolate(),
+        right = Operators.Extrapolate(),
+    )
     A = Operators.AdvectionC2C(
         left = Operators.SetValue(sin(-t)),
         right = Operators.Extrapolate(),
     )
-    return @. dθ = -A(V, θ) + fcc(V,θ)
+    return @. dθ = -A(V, θ) + fcc(V, θ)
 end
 
 # use the advection operator
@@ -93,10 +113,10 @@ end
 @show tendency1!(similar(θ), θ, nothing, 0.0)
 # Solve the ODE operator
 Δt = 0.001
-prob1 = ODEProblem(tendency1!, θ, (0.0, 10.0))
-prob2 = ODEProblem(tendency2!, θ, (0.0, 10.0))
-prob3 = ODEProblem(tendency3!, θ, (0.0, 10.0))
-prob4 = ODEProblem(tendency4!, θ, (0.0, 10.0))
+prob1 = ODEProblem(tendency1!, θ, (0.0, 5.0))
+prob2 = ODEProblem(tendency2!, θ, (0.0, 5.0))
+prob3 = ODEProblem(tendency3!, θ, (0.0, 5.0))
+prob4 = ODEProblem(tendency4!, θ, (0.0, 5.0))
 sol1 = solve(
     prob1,
     SSPRK33(),
@@ -134,54 +154,54 @@ ENV["GKSwstype"] = "nul"
 import Plots
 Plots.GRBackend()
 
-dirname = "advect"
+dirname = "advect_step_function"
 path = joinpath(@__DIR__, "output", dirname)
 mkpath(path)
 
 anim = Plots.@animate for u in sol1.u
     Plots.plot(u, xlim = (-1, 1))
 end
-Plots.mp4(anim, joinpath(path, "UBP_advect.mp4"), fps = 10)
+Plots.mp4(anim, joinpath(path, "UBP_advect_step_function.mp4"), fps = 10)
 Plots.png(
     Plots.plot(sol1.u[end], xlim = (-1, 1)),
-    joinpath(path, "sol1_advect_end.png"),
+    joinpath(path, "sol1_advect_step_function_end.png"),
 )
 
 anim = Plots.@animate for u in sol2.u
     Plots.plot(u, xlim = (-1, 1))
 end
-Plots.mp4(anim, joinpath(path, "UBP_advect_fc.mp4"), fps = 10)
+Plots.mp4(anim, joinpath(path, "UBP_advect_step_function_fc.mp4"), fps = 10)
 Plots.png(
     Plots.plot(sol2.u[end], xlim = (-1, 1)),
-    joinpath(path, "sol2_advect_end.png"),
+    joinpath(path, "sol2_advect_step_function_end.png"),
 )
 
 anim = Plots.@animate for u in sol3.u
     Plots.plot(u, xlim = (-1, 1))
 end
-Plots.mp4(anim, joinpath(path, "C2C_advect.mp4"), fps = 10)
+Plots.mp4(anim, joinpath(path, "C2C_advect_step_function.mp4"), fps = 10)
 Plots.png(
     Plots.plot(sol3.u[end], xlim = (-1, 1)),
-    joinpath(path, "sol3_advect_end.png"),
+    joinpath(path, "sol3_advect_step_function_end.png"),
 )
 
 anim = Plots.@animate for u in sol4.u
     Plots.plot(u, xlim = (-1, 1))
 end
-Plots.mp4(anim, joinpath(path, "C2C_advect_fc.mp4"), fps = 10)
+Plots.mp4(anim, joinpath(path, "C2C_advect_step_function_fc.mp4"), fps = 10)
 Plots.png(
     Plots.plot(sol4.u[end], xlim = (-1, 1)),
-    joinpath(path, "sol4_advect_end.png"),
+    joinpath(path, "sol4_advect_step_function_end.png"),
 )
 
-p = Plots.plot(sol1.u[end], xlim = (-1, 1), ls=:dash, label = "UBP")
-p = Plots.plot!(sol2.u[end], xlim = (-1, 1), ls=:dot, label = "UBP_FC")
-p = Plots.plot!(sol3.u[end], xlim = (-1, 1), ls=:solid, label = "C2C") 
-p = Plots.plot!(sol4.u[end], xlim = (-1, 1), ls=:dashdot, label = "C2C_FC")
-Plots.png(
-    p,
-    joinpath(path, "all_advect_end.png"),
-)
+p = Plots.plot(sol1.u[end], xlim = (-1, 1), label = "UBP")
+p = Plots.plot!(sol2.u[end], xlim = (-1, 1), label = "UBP_FC")
+p = Plots.plot!(sol3.u[end], xlim = (-1, 1), label = "C2C")
+p = Plots.plot!(sol4.u[end], xlim = (-1, 1), label = "C2C_FC")
+Plots.png(p, joinpath(path, "all_advect_step_function_end.png"))
+
+
+
 
 function linkfig(figpath, alt = "")
     # buildkite-agent upload figpath
@@ -192,4 +212,4 @@ function linkfig(figpath, alt = "")
     end
 end
 
-linkfig("output/$(dirname)/advect_end.png", "Advect End Simulation")
+linkfig("output/$(dirname)/advect_step_function_end.png", "Advect End Simulation")
